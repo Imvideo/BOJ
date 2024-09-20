002cbbc16df189289c37ff09e386d21cb9813edf
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2024-09-20 20:56:04 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n,m;
+    static int[] arr = new int[10];
+    static boolean[] isused = new boolean[10];
+
+    static void func(int k) throws IOException{
+        if(k == m){
+            for(int i=0; i<m; i++){
+                bw.write(arr[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+        int st = 1;
+        if(k != 0) st = arr[k-1] + 1;
+        for(int i=st; i<=n; i++){
+            if(!isused[i]){
+                arr[k] = i;
+                isused[i] = true;
+                func(k+1);
+                isused[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        func(0);
+        bw.flush();
+        bw.close();
+    }
+}
