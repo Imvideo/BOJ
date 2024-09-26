201bfa8b28b8e73080fe891217bb911bb284bd3d
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2024-09-26 19:18:02 +0900

    [Silver III] Title: N과 M (8), Time: 172 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int n,m;
+    static int[] arr = new int[10];
+    static int[] num;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static void func(int k) throws IOException{
+        if(k==m){
+            for(int i=0; i<k; i++){
+                bw.write(arr[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i=0; i<n; i++){
+            if(k!=0){
+                if(arr[k-1] > num[i]) continue;
+            }
+            arr[k] = num[i];
+            func(k+1);
+        }
+        return;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        func(0);
+        bw.flush();
+        bw.close();
+    }
+}
