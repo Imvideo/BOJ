389411d9da2fc2a1c14756878d110819fd590ad1
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-05-11 20:28:45 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,25 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <queue>
 
-int main(void)
-{
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  queue<int> Q;
-  int N,num;
-  cin >> N;
-  for (int i = 1; i <=N; i++){
-    Q.push(i);
+using namespace std;
+int n;
+queue<int> q;
+int main(){
+  cin >> n;
+  for(int i=1; i<=n; i++){
+    q.push(i);
   }
-  while(Q.size()!=1){
-    Q.pop();
-    if(Q.size()!=1){
-      num=Q.front();
-      Q.push(num);
-      Q.pop();
-    }
-    else break;
+  while(q.size()>1){
+    q.pop();
+    int num = q.front();
+    q.pop();
+    q.push(num);
   }
-  cout << Q.front() ;
-  return 0;
+  cout << q.front();
 }
