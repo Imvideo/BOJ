1b50ca7bcad2a7d85e4b5af3c614cd22d5d24ae9
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-05-23 01:52:12 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2128 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,54 +1,77 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <queue>
+#include <utility>
 #define X first
 #define Y second
-string board[102]; 
-bool vis[102][102];           
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-queue<pair<int,int> > Q;
-int n;
-void bfs(int x,int y){
-  vis[x][y] = true;
-  Q.push({x,y});
-  while (!Q.empty()) {
-      auto cur = Q.front(); Q.pop();
-      for (int dir =0; dir <4;dir++){
-        int nx = cur.X + dx[dir];
-        int ny = cur.Y + dy[dir];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; //구역이 n*n인 정사각형이므로
-        if (vis[nx][ny] || board[nx][ny] != board[x][y]) continue;  //서로 다른 구역이면 스킵
-        vis[nx][ny] =true;
-        Q.push({nx,ny});
-      }
-    }
-}
-int area(){
-  int cnt = 0;
-  for (int i =0 ; i<n; i++){
-    for (int j =0; j<n; j++){
-      if (!vis[i][j]){
-        cnt++;
-        bfs(i,j);
-      }
-    }
-  }
-  return cnt;
-}
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
 
-  cin >> n;
-  for (int i = 0; i < n; i++) cin>> board[i];
-  int not_g = area(); //적록색약이 아닌 사람
-  for(int i=0;i<n;i++) fill(vis[i],vis[i]+n,false);
-  for (int i=0;i<n;i++){
-    for (int j =0;j<board[i].length();j++){
-      if (board[i][j] == 'G') board[i][j] = 'R';    //적록색약은 빨강과 초록을 구분못하니 초록을 빨강으로 바꿔줌
-    }
-  }
-  int is_g = area(); //적록색약인 사람
-  cout << not_g << " "<<is_g;
-  return 0;
+using namespace std;
+int n, num1 = 0, num2 = 0;
+char board1[102][102]; // !적록색약
+char board2[102][102]; // 적록색약
+int vis1[102][102]; // !적록색약
+int vis2[102][102];	// 적록색약
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+queue<pair<int, int>> q;
+int main() {
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board1[i][j];
+			if (board1[i][j] == 'G') {
+				board2[i][j] = 'R';
+			}
+			else {
+				board2[i][j] = board1[i][j];
+			}
+		}
+		fill(vis1[i], vis1[i] + n, -1);
+		fill(vis2[i], vis2[i] + n, -1);
+	}
+	
+	// !적록색약
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis1[i][j] != -1) continue;
+			q.push({ i,j });
+			char color = board1[i][j];
+			vis1[i][j] = 0;
+			num1++;
+			while (!q.empty()) {
+				pair<int, int> cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.X + dx[dir];
+					int ny = cur.Y + dy[dir];
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board1[nx][ny] != color || vis1[nx][ny] != -1) continue;
+					vis1[nx][ny] = 0;
+					q.push({ nx,ny });
+				}
+			}
+		}
+	}
+
+	// 적록색약
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis2[i][j] != -1) continue;
+			q.push({ i,j });
+			char color = board2[i][j];
+			vis2[i][j] = 0;
+			num2++;
+			while (!q.empty()) {
+				pair<int, int> cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.X + dx[dir];
+					int ny = cur.Y + dy[dir];
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board2[nx][ny] != color || vis2[nx][ny] != -1) continue;
+					vis2[nx][ny] = 0;
+					q.push({ nx,ny });
+				}
+			}
+		}
+	}
+	cout << num1 << " " << num2;
 }
