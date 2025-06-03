08af19faa635840b9d02ec2437a65b90b76be843
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-06-03 10:47:16 +0900

    [Silver I] Title: 나이트의 이동, Time: 24 ms, Memory: 2376 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,37 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <utility>
+
 using namespace std;
+bool flag;
+int T, l, n, m, x, y;
+int board[302][302];
+int dx[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int dy[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
-#define X first
-#define Y second
-int dist[305][305];
-int dx[8] = {2, 1, -1, -2, -2, -1, 1, 2};     //나이트가 이동할 수 있는 8가지 경우의 수
-int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-int t, n, x, y, xx, yy;
-queue <pair<int, int >> Q;
+int main() {
+	cin >> T;
+	while (T--) {
+		cin >> l >> n >> m >> x >> y;
+		for (int i = 0; i < l; i++) {
+			fill(board[i], board[i] + l, -1);
+		}
+		flag = false;
+		queue <pair<int, int>> q;
+		board[n][m] = 0;
+		q.push({ n,m });
+		if (n == x && m == y) {
+			cout << board[n][m] << "\n";
+			continue;
+		}
+		while (!q.empty()) {
+			if (flag) break;
+			auto cur = q.front(); q.pop();
+			for (int dir = 0; dir < 8; dir++) {
+				int nx = cur.first + dx[dir];
+				int ny = cur.second + dy[dir];
+				if (nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
+				if (board[nx][ny] > -1) continue;
+				board[nx][ny] = board[cur.first][cur.second] + 1;
+				q.push({ nx,ny });
+				if (nx == x && ny == y) {
+					flag = true;
+					cout << board[nx][ny] << "\n";
+					break;
+				}
+			}
+		}
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> t;
-  while (t--) {
-    cin >> n;
-    for (int i = 0; i < n; i++) fill(dist[i], dist[i] + n, -1);
-    cin >> x >> y;
-    dist[x][y] = 0;
-    Q.push({x, y});
-    cin >> xx >> yy;
-    while (!Q.empty()) {
-      auto cur = Q.front(); Q.pop();
-      for (int dir = 0; dir < 8; dir++) {
-        int nx = cur.X + dx[dir];
-        int ny = cur.Y + dy[dir];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        if (dist[nx][ny] >= 0) continue;
-        dist[nx][ny] = dist[cur.X][cur.Y] + 1;        
-        Q.push({nx, ny});
-      }
-    }
-    cout << dist[xx][yy] << "\n";
-  }
+	}
 }
