e759e67542e1788d6e80c1048d919feb6c23d11c
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-06-09 20:33:51 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1016 ms, Memory: 305016 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,49 +1,49 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main{
-    static int[] arr = new int[100005];
-    static int[] state = new int[100005];
+    static int[] arr = new int[100002];
+    static int[] vis = new int[100002];
 
     static void run(int x){
         int cur = x;
         while(true){
-            state[cur] = x;
+            vis[cur] = x;
             cur = arr[cur];
-            //이번 방문에서 지나간 학생에 도달한 경우
-            if(state[cur] == x){
-                while(state[cur] != -1){
-                    state[cur] = -1;
+        
+            if(vis[cur] == x){
+                while(vis[cur]!=-1){
+                    vis[cur] = -1;
                     cur = arr[cur];
                 }
                 return;
             }
-            // 이전 방문에서 지나간 학생에 도달한 경우
-            else if(state[cur] != 0) return; 
+            else if(vis[cur] != 0) return;
         }
     }
+
     public static void main(String[] args) throws IOException {
+        int t, ans=0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
-        
-        for(int t=0; t<T; t++) {
+        t = Integer.parseInt(br.readLine());
+        for(int T=0; T<t; T++){
             int n = Integer.parseInt(br.readLine());
-            
-            for(int i=1;i<=n;i++) {
-                state[i] = 0;
-            }
+            ans = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=1; i<=n; i++) arr[i] = Integer.parseInt(st.nextToken());
-            for(int i=1; i<=n; i++){
-                if(state[i] == 0) run(i);
+            for(int i=1;i<=n;i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+                vis[i] = 0;
             }
-            int cnt = 0;
             for(int i=1; i<=n; i++){
-                if(state[i] != -1) cnt++;
+                if(vis[i] == 0) run(i);
             }
-            bw.write(cnt+"\n");
+            for(int i=1; i<=n; i++){
+                if(vis[i] != - 1) ans++;
+            }
+            bw.write( ans+"\n");
+            
         }
         bw.flush();
         bw.close();
