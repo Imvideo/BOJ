a39878fd82da01e94aeae44c094d06107282a438
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-10-30 22:44:36 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,61 +2,55 @@ import java.io.*;
 import java.util.*;
 import java.awt.Point;
 
-public class Main{
+public class Main {
+    static int dx[] = {1,0,-1,0};
+    static int dy[] = {0,1,0,-1};
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T, n, m, num, area;
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        T = Integer.parseInt(br.readLine());
+        
+        int T = Integer.parseInt(br.readLine());
         for(int t=0; t<T; t++){
+            int m,n,k, cnt=0;
+            boolean[][] visited = new boolean[52][52];
             StringTokenizer st = new StringTokenizer(br.readLine());
             m = Integer.parseInt(st.nextToken());
             n = Integer.parseInt(st.nextToken());
-            int[][] map = new int[n][m];
-            int[][] dist = new int[n][m];
-            
-            for(int x = 0 ; x < n ; x++){
-                Arrays.fill(map[x], 0);
-                Arrays.fill(dist[x], -1);
-            }
-
-            num = Integer.parseInt(st.nextToken());
-            area = 0;
-
-            for(int x = 0; x < num; x++){ // 배추 위치 입력
+            k = Integer.parseInt(st.nextToken());
+            int[][] board = new int[52][52];
+            for(int i=0; i<k; i++){
                 st = new StringTokenizer(br.readLine());
-                int b = Integer.parseInt(st.nextToken());
-                int a = Integer.parseInt(st.nextToken());
-                map[a][b] = 1;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                board[y][x] = 1;
             }
-
-            Queue<Point> queue = new LinkedList<Point>();
             for(int i=0; i<n; i++){
                 for(int j=0; j<m; j++){
-                    if(map[i][j] == 0 || dist[i][j] != -1) continue;
-                    area++;
-                    dist[i][j] = 0;
-                    queue.add(new Point(i,j));
-                    while(!queue.isEmpty()){
-                        Point point = queue.poll();
-                        for(int k = 0; k<4; k++){
-                            int nx = point.x + dx[k];
-                            int ny = point.y + dy[k];
-                            if(nx < 0 || nx >=n || ny <0 || ny >=m) continue;
-                            if(map[nx][ny] != 1 || dist[nx][ny] != -1) continue;
-                            dist[nx][ny] = 0;
-                            queue.add(new Point(nx,ny));
+                    if(visited[i][j]) continue;
+                    visited[i][j] = true;
+                    if(board[i][j] == 0) continue;
+                    Queue<Point> q = new LinkedList<>();
+                    q.add(new Point(i,j));
+                    cnt++;
+                    while(!q.isEmpty()){
+                        Point cur = q.poll();
+                        for(int d=0; d<4; d++){
+                            int nx = cur.x + dx[d];
+                            int ny = cur.y + dy[d];
+                            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                            if(visited[nx][ny]) continue;
+                            visited[nx][ny] = true;
+                            if(board[nx][ny] == 0) continue;
+                            q.add(new Point(nx,ny));
                         }
                     }
                 }
             }
-
-            bw.write(area+"\n");
-
+            bw.write(cnt+"\n");
+            
         }
+
         bw.flush();
         bw.close();
     }
