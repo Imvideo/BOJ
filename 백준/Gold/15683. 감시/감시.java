7f3b464d86ec8e779baff46a48cdae39a32faab8
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-11-15 18:10:55 +0900

    [Gold III] Title: 감시, Time: 332 ms, Memory: 26688 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -2,92 +2,90 @@ import java.io.*;
 import java.util.*;
 import java.awt.Point;
 
-public class Main{
-    static int[][] board1 = new int[10][10];
-    static int[][] board2 = new int[10][10];
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int n,m,ans=0;
-    static List<Point> cctv = new ArrayList<Point>();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static boolean OOB(int a, int b){
-        return a < 0 || a>=n || b<0 || b>=m;
-    }
+public class Main {
+    static int board[][] = new int[10][10];
+    static int board2[][] = new int[10][10];
+    static int dx[] = {-1,0,1,0};
+    static int dy[] = {0,1,0,-1};
+    static int n, m, mn = 0;
+    static ArrayList<Point> cam = new ArrayList<>();
     
-    static void upd(int x, int y, int dir){
-        dir%=4;
+    static void func(int x, int y, int dir){
+        int nx = x, ny = y;
         while(true){
-            x+=dx[dir];
-            y+=dy[dir];
-            if(OOB(x,y) || board2[x][y] == 6) return;
-            if(board2[x][y] != 0) continue;
-            board2[x][y] = 7;
+            nx+=dx[dir]; ny+=dy[dir];
+            if(nx < 0 || nx >=n || ny < 0 || ny >= m || board2[nx][ny] == 6) break;
+            if(board2[nx][ny] != 0) continue;
+            board2[nx][ny] = 7;
+            
         }
+        return;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<m; j++){
-                board1[i][j] = Integer.parseInt(st.nextToken());
-                if(board1[i][j] != 0 && board1[i][j] != 6){
-                    cctv.add(new Point(i,j));
+                int num = Integer.parseInt(st.nextToken());
+                board[i][j] = num;
+                if(num != 0 && num != 6) {
+                    cam.add(new Point(i,j));
                 }
-                if(board1[i][j] ==0) ans++;
+                if(num == 0) mn++;
             }
         }
-        for(int tmp=0; tmp<(1<<(2*cctv.size()));tmp++){
+        for(int tmp=0; tmp<1<<(2*cam.size()); tmp++){
             for(int i=0; i<n; i++){
                 for(int j=0; j<m; j++){
-                    board2[i][j] = board1[i][j];
+                    board2[i][j] = board[i][j];
                 }
             }
             int brute = tmp;
-            for(int i=0; i<cctv.size(); i++){
-                int dir = brute % 4;
+            for(int i=0; i<cam.size(); i++){
+                int dir = brute%4;
                 brute /= 4;
-                Point point = cctv.get(i);
-                int x = point.x;
-                int y = point.y;
-                if(board1[x][y] == 1){
-                    upd(x,y,dir);
+                int nx = cam.get(i).x;
+                int ny = cam.get(i).y;
+                if(board[nx][ny] == 1){
+                    func(nx, ny, dir);
                 }
-                else if(board1[x][y] == 2){
-                    upd(x,y,dir);
-                    upd(x,y,dir+2);
+                else if(board[nx][ny] == 2){
+                    func(nx, ny, dir);
+                    func(nx, ny, (dir+2)%4);
                 }
-                else if(board1[x][y] == 3){
-                    upd(x,y,dir);
-                    upd(x,y,dir+1);
+                else if(board[nx][ny] == 3){
+                    func(nx, ny, dir);
+                    func(nx, ny, (dir+1)%4);
                 }
-                else if(board1[x][y] == 4){
-                    upd(x,y,dir);
-                    upd(x,y,dir+1);
-                    upd(x,y,dir+2);
+                else if(board[nx][ny] == 4){
+                    func(nx, ny, dir);
+                    func(nx, ny, (dir+1)%4);
+                    func(nx, ny, (dir+2)%4);
                 }
-                else if(board1[x][y] == 5){
-                    upd(x,y,dir);
-                    upd(x,y,dir+1);
-                    upd(x,y,dir+2);
-                    upd(x,y,dir+3);
+                else if(board[nx][ny] == 5){
+                    func(nx, ny, dir);
+                    func(nx, ny, (dir+1)%4);
+                    func(nx, ny, (dir+2)%4);
+                    func(nx, ny, (dir+3)%4);
                 }
+
             }
-            int val = 0;
+            int cnt = 0;
             for(int i=0; i<n; i++){
                 for(int j=0; j<m; j++){
-                    if(board2[i][j] == 0) val++;
+                    if(board2[i][j] == 0) cnt++;
                 }
             }
-            ans = Math.min(val,ans);
+            mn = Math.min(mn, cnt);
         }
-        bw.write(ans+"");
+        bw.write(mn+"");
         bw.flush();
         bw.close();
-
     }
 }
