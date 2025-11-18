a290f5b7b441141c5eaef49b2c9580f33783ff23
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-11-18 11:45:09 +0900

    [Gold V] Title: 로봇 청소기, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,22 +3,22 @@ import java.util.*;
 import java.awt.Point;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int n, m, r, c, dir, cnt = 0;
+    static int[][] board = new int[52][52];
+    static boolean[][] visited = new boolean[52][52];
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+    static Queue<State> q = new LinkedList<>();
     static class State{
         int r, c, dir;
-        State(int r, int c, int d){
-            this.r = r; this.c = c; this.dir = d;
+        State(int r, int c, int dir){
+            this.r = r; this.c = c; this.dir = dir;
         }
     }
-    static int cnt=0;
-    static int board[][] = new int[52][52];
-    static boolean visited[][] = new boolean[52][52];
-    static int dx[] = {1,0,-1,0};
-    static int dy[] = {0,-1,0,1};
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n, m, r, c, dir;
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -32,42 +32,40 @@ public class Main {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        Queue<State> q = new LinkedList<>();
+        
         q.add(new State(r, c, dir));
-
         while(!q.isEmpty()){
+            boolean cleaned = true;
             State cur = q.poll();
-            boolean flag = false;
-            if(board[cur.r][cur.c] == 0 && !visited[cur.r][cur.c]){
+            if(!visited[cur.r][cur.c]) {
                 visited[cur.r][cur.c] = true;
                 cnt++;
             }
             for(int d=0; d<4; d++){
                 int nx = cur.r + dx[d];
                 int ny = cur.c + dy[d];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(visited[nx][ny] || board[nx][ny] == 1) continue;
-                flag = true;
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) continue;
+                if(visited[nx][ny]) continue;
+                cleaned = false;
             }
-            if(!flag) {
-                int nx = cur.r + dx[cur.dir];
-                int ny = cur.c + dy[cur.dir];
+            if(cleaned){
+                int nx = cur.r + dx[(cur.dir+2)%4];
+                int ny = cur.c + dy[(cur.dir+2)%4];
                 if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) break;
                 q.add(new State(nx, ny, cur.dir));
             }
             else{
-                for(int i=0; i<4; i++){
-                int tmpDir = (cur.dir+3*(i+1))%4;
-                int nx = cur.r + dx[(tmpDir+2)%4];
-                int ny = cur.c + dy[(tmpDir+2)%4];
-                if(board[nx][ny] == 0 && !visited[nx][ny]){
-                    q.add(new State(nx, ny, tmpDir));
+                int tmp = cur.dir;
+                for(int d=0; d<4; d++){
+                    tmp = (tmp + 3) % 4;
+                    int nx = cur.r + dx[tmp];
+                    int ny = cur.c + dy[tmp];
+                    if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) continue;
+                    if(visited[nx][ny]) continue;
+                    q.add(new State(nx, ny, tmp));
                     break;
                 }
             }
-            }
-            
-            
         }
         bw.write(cnt+"");
         bw.flush();
