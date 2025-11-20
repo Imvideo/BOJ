3831d334528de67795381c0b3579d2252a99b87b
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-11-20 19:20:29 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -5,20 +5,20 @@ import java.awt.Point;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
+
     static int n, m, r, c, dir, cnt = 0;
+    static boolean flag = false;
     static int[][] board = new int[52][52];
-    static boolean[][] visited = new boolean[52][52];
+    static boolean[][] visit = new boolean[52][52];
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
-    static Queue<State> q = new LinkedList<>();
-    static class State{
-        int r, c, dir;
-        State(int r, int c, int dir){
-            this.r = r; this.c = c; this.dir = dir;
-        }
+
+    static boolean OOB(int nx, int ny){
+        if(nx < 0 || nx >=n || ny < 0 || ny >= m) return true;
+        return false;
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
+        // 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -32,38 +32,36 @@ public class Main {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        q.add(new State(r, c, dir));
-        while(!q.isEmpty()){
-            boolean cleaned = true;
-            State cur = q.poll();
-            if(!visited[cur.r][cur.c]) {
-                visited[cur.r][cur.c] = true;
+        // 
+        while(true){
+            flag = false;
+            if(!visit[r][c]) {
+                visit[r][c] = true; // 1. 현재칸 청소;
                 cnt++;
             }
+            
             for(int d=0; d<4; d++){
-                int nx = cur.r + dx[d];
-                int ny = cur.c + dy[d];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) continue;
-                if(visited[nx][ny]) continue;
-                cleaned = false;
+                int nx = r + dx[d];
+                int ny = c + dy[d];
+                if(OOB(nx, ny) || board[nx][ny] == 1 || visit[nx][ny]) continue;
+                flag = true;
             }
-            if(cleaned){
-                int nx = cur.r + dx[(cur.dir+2)%4];
-                int ny = cur.c + dy[(cur.dir+2)%4];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) break;
-                q.add(new State(nx, ny, cur.dir));
+            if(!flag){ // 4방향에 청소할 곳 없음
+                int nx = r + dx[(dir+2)%4];
+                int ny = c + dy[(dir+2)%4];
+                if(OOB(nx, ny) || board[nx][ny] == 1) break;
+                r = nx; c = ny;
             }
-            else{
-                int tmp = cur.dir;
+            else{ // 4방향에 청소할 곳 있음
                 for(int d=0; d<4; d++){
-                    tmp = (tmp + 3) % 4;
-                    int nx = cur.r + dx[tmp];
-                    int ny = cur.c + dy[tmp];
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 1) continue;
-                    if(visited[nx][ny]) continue;
-                    q.add(new State(nx, ny, tmp));
+                    dir = (dir+3) %4;
+                    int nx = r + dx[dir];
+                    int ny = c + dy[dir];
+                    if(OOB(nx, ny) || board[nx][ny] == 1 || visit[nx][ny]) continue;
+                    r = nx;
+                    c = ny;
                     break;
+                
                 }
             }
         }
