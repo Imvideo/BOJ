5ff2bfe8e9d23e2bcd7cc10a10b3c3714da80729
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-11-20 17:58:40 +0900

    [Gold III] Title: 감시, Time: 316 ms, Memory: 23684 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -3,88 +3,87 @@ import java.util.*;
 import java.awt.Point;
 
 public class Main {
-    static int board[][] = new int[10][10];
-    static int board2[][] = new int[10][10];
-    static int dx[] = {-1,0,1,0};
-    static int dy[] = {0,1,0,-1};
-    static int n, m, mn = 0;
-    static ArrayList<Point> cam = new ArrayList<>();
-    
-    static void func(int x, int y, int dir){
-        int nx = x, ny = y;
-        while(true){
-            nx+=dx[dir]; ny+=dy[dir];
-            if(nx < 0 || nx >=n || ny < 0 || ny >= m || board2[nx][ny] == 6) break;
-            if(board2[nx][ny] != 0) continue;
-            board2[nx][ny] = 7;
-            
-        }
-        return;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        
+    static int n, m, camNum = 0, cnt, zero = 0, mn;
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+    static int[][] board = new int[10][10];
+    static int[][] tmp = new int[10][10];
+    static ArrayList<Point> cam = new ArrayList<>();
+
+    static void func(int x, int y, int dir) {
+        while (true) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || tmp[nx][ny] == 6)
+                break;
+            if (tmp[nx][ny] == 0) {
+                tmp[nx][ny] = 7;
+                cnt++;
+            }
+            x = nx;
+            y = ny;
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        // 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++){
-                int num = Integer.parseInt(st.nextToken());
-                board[i][j] = num;
-                if(num != 0 && num != 6) {
-                    cam.add(new Point(i,j));
-                }
-                if(num == 0) mn++;
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] != 0 && board[i][j] != 6)
+                    cam.add(new Point(i, j));
+                if (board[i][j] == 0)
+                    zero++;
             }
         }
-        for(int tmp=0; tmp<1<<(2*cam.size()); tmp++){
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    board2[i][j] = board[i][j];
+        mn = zero;
+        //
+        for (int i = 0; i < (1 << (2 * cam.size())); i++) {
+            int k = i;
+            cnt = 0;
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < m; y++) {
+                    tmp[x][y] = board[x][y];
                 }
             }
-            int brute = tmp;
-            for(int i=0; i<cam.size(); i++){
-                int dir = brute%4;
-                brute /= 4;
-                int nx = cam.get(i).x;
-                int ny = cam.get(i).y;
-                if(board[nx][ny] == 1){
-                    func(nx, ny, dir);
-                }
-                else if(board[nx][ny] == 2){
-                    func(nx, ny, dir);
-                    func(nx, ny, (dir+2)%4);
-                }
-                else if(board[nx][ny] == 3){
-                    func(nx, ny, dir);
-                    func(nx, ny, (dir+1)%4);
-                }
-                else if(board[nx][ny] == 4){
-                    func(nx, ny, dir);
-                    func(nx, ny, (dir+1)%4);
-                    func(nx, ny, (dir+2)%4);
-                }
-                else if(board[nx][ny] == 5){
-                    func(nx, ny, dir);
-                    func(nx, ny, (dir+1)%4);
-                    func(nx, ny, (dir+2)%4);
-                    func(nx, ny, (dir+3)%4);
+            for (int j = 0; j < cam.size(); j++) {
+                int r, c, num;
+                int dir = k % 4;
+                k /= 4;
+                r = cam.get(j).x;
+                c = cam.get(j).y;
+                num = board[r][c];
+                if (num == 1) {
+                    func(r, c, dir);
+                } else if (num == 2) {
+                    func(r, c, dir);
+                    func(r, c, (dir + 2) % 4);
+                } else if (num == 3) {
+                    func(r, c, dir);
+                    func(r, c, (dir + 1) % 4);
+                } else if (num == 4) {
+                    func(r, c, dir);
+                    func(r, c, (dir + 1) % 4);
+                    func(r, c, (dir + 2) % 4);
+                } else if (num == 5) {
+                    func(r, c, dir);
+                    func(r, c, (dir + 1) % 4);
+                    func(r, c, (dir + 2) % 4);
+                    func(r, c, (dir + 3) % 4);
                 }
 
             }
-            int cnt = 0;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    if(board2[i][j] == 0) cnt++;
-                }
-            }
-            mn = Math.min(mn, cnt);
+            int area = zero - cnt;
+            mn = Math.min(mn, area);
         }
-        bw.write(mn+"");
+        bw.write(mn + "");
         bw.flush();
         bw.close();
     }
