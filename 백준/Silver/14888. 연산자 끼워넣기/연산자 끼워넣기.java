8931ac70420b810c802448b6b0fc22e4019fbcfe
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-11-20 14:52:52 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 136 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -5,42 +5,40 @@ import java.awt.Point;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
-    static int n, mx = -1000000001, mn = 1000000001, opCnt = 0;
-    static int[] arr = new int[12];
+
+    static int N, mx = Integer.MIN_VALUE, mn = Integer.MAX_VALUE;
+    static int[] num = new int[12];
     static int[] op = new int[4];
 
     static void func(int k, int sum){
-        if(k == n-1){
+        if(k == N){
             mx = Math.max(mx, sum);
             mn = Math.min(mn, sum);
             return;
         }
-        for(int i=0; i< 4; i++){
-            if(op[i] > 0){
-                op[i]--;
-                if(i == 0) func(k+1, sum + arr[k+1]);
-                else if(i == 1) func(k+1, sum - arr[k+1]);
-                else if(i == 2) func(k+1, sum * arr[k+1]);
-                else if(i == 3) func(k+1, sum / arr[k+1]);
-                op[i]++;
-            }
+        for(int i=0; i<4; i++){
+            if(op[i] == 0) continue;
+            op[i]--;
+            if(i == 0) func(k+1, sum + num[k]);
+            else if(i == 1) func(k+1, sum - num[k]);
+            else if(i == 2) func(k+1, sum * num[k]);
+            else if(i == 3) func(k+1, sum / num[k]);
+            op[i]++;
         }
     }
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
+    public static void main(String args[]) throws IOException {
+        // 입력
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<4; i++){
             op[i] = Integer.parseInt(st.nextToken());
-            opCnt += op[i];
         }
-        func(0, arr[0]);
-        bw.write(mx + "\n" + mn);
+        func(1,num[0]);
+        bw.write(mx+"\n"+mn);
         bw.flush();
         bw.close();
     }
