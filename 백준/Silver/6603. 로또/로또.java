5c8bdf64813bb12aa25e33dbd5c10847c26ce1cf
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2025-12-16 11:34:23 +0900

    [Silver II] Title: 로또, Time: 128 ms, Memory: 15164 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,44 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n;
-    static int[] num = new int[15];
-    static int[] arr = new int[8];
-    static boolean[] isused = new boolean[15];
-    static void func(int k) throws IOException{
-        if(k==6){
+    
+    static int k;
+    static int[] num;
+    static int[] ans;
+    static void func(int x, int idx) throws IOException {
+        if(x == 6){
             for(int i=0; i<6; i++){
-                bw.write(arr[i] + " ");
+                bw.write(ans[i]+" ");
             }
             bw.write("\n");
             return;
         }
-        for(int i=0; i<n; i++){
-            if(!isused[i]){
-                if(k!=0 && arr[k-1] > num[i]) continue;
-                arr[k] = num[i];
-                isused[i] = true;
-                func(k+1);
-                isused[i] = false;
-            }
+        for(int i = idx; i<k; i++){
+            ans[x] = num[i];
+            func(x+1, i+1);
         }
-        return;
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
         while(true){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            if(n==0) break;
-            for(int i=0; i<n; i++){
+            k = Integer.parseInt(st.nextToken());
+            if(k == 0) break;
+            num = new int[k];
+            ans = new int[6];
+            for(int i=0; i<k; i++){
                 num[i] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(num,0,n);
-            func(0);
+            func(0,0);
             bw.write("\n");
+
         }
+
         bw.flush();
         bw.close();
     }
