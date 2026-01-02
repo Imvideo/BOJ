a09217d920ef220f6328fd1a1f8cc071141d7aee
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2026-01-02 14:34:48 +0900

    [Gold III] Title: 스티커 붙이기, Time: 156 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
@@ -7,41 +7,41 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int n, m, k, r, c;
+    static int[][] sticker;
     static int[][] board = new int[42][42];
-    static int[][] sticker = new int[12][12];
-
-    static boolean canPatch(int x, int y){
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(sticker[i][j] == 1 && board[x+i][y+j] == 1) return false;
-            }
-        }
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(sticker[i][j] == 1) board[x+i][y+j] = 1;
-            }
-        }
-        return true;
-    }
 
     static boolean patch(){
         for(int i=0; i<=n-r; i++){
-            for(int j=0; j<=m-c;j++){
-                if(canPatch(i,j)) return true;;
+            for(int j=0; j<=m-c; j++){
+                if(canPatch(i, j)) return true;;
             }
         }
         return false;
     }
 
+    static boolean canPatch(int x, int y){
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i+x][j+y] == 1 && sticker[i][j] == 1) return false;
+            }
+        }
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(sticker[i][j] == 1) board[i+x][j+y] = 1;
+            }
+        }
+        return true;
+    }
+
     static void rotate(){
         int[][] tmp = new int[12][12];
-        for(int i=0;i<r;i++){
+        for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 tmp[j][r-i-1] = sticker[i][j];
             }
         }
-        int temp = r; r = c; c = temp;
-        for(int i=0;i<r;i++){
+        int t = r; r = c; c = t;
+        for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 sticker[i][j] = tmp[i][j];
             }
@@ -52,29 +52,30 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        for(int t=0; t<k; t++){
+
+        for(int K=0; K<k; K++){
             st = new StringTokenizer(br.readLine());
             r = Integer.parseInt(st.nextToken());
             c = Integer.parseInt(st.nextToken());
-            for(int i=0; i<r; i++){
+            sticker = new int[12][12];
+            for(int i=0 ;i<r; i++){
                 st = new StringTokenizer(br.readLine());
-                for(int j=0; j<c; j++) {
+                for(int j=0; j<c; j++){
                     sticker[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            for(int i=0; i<4; i++){
-                if(i!=0) rotate();
+            for(int t=0; t<4; t++){
+                if(t!=0) rotate();
                 if(patch()) break;
             }
         }
         int cnt = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == 1) cnt++;
+        for(int i=0 ;i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(board[i][j] == 1) cnt++;
+                }
             }
-        }
-        bw.write(cnt+"\n");
-        
+        bw.write(cnt+"");
         bw.flush();
         bw.close();
     }
