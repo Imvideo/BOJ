06749059969dcfc8314a89f7001d3853fa537f76
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2026-01-12 22:59:07 +0900

    [Gold V] Title: 톱니바퀴, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,75 +1,76 @@
 import java.io.*;
-import java.util.*;
-import java.awt.Point;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[][] gear = new int[4][8];
-    static int[] idx = new int[4];
-    static int[] rot;
-    static ArrayList<Point> arr = new ArrayList<>();
     static int k;
+    static int[][] gear = new int[4][8];
 
-    static void rotate(int gearNum, int dir){ 
-        //왼쪽으로 전파
-        for(int i=gearNum; i>0; i--){
-            int rightOfLeft = gear[i-1][idx[i-1]];
-            int leftOfRight = gear[i][(idx[i]+4)%8];
-            if(rightOfLeft == leftOfRight) break;
-            rot[i-1] = -rot[i];
+    static void go(int x, int dir){
+        int[] dirs = new int[4];
+        dirs[x] = dir;
+        int idx = x;
+        while(idx > 0 && gear[idx][6] != gear[idx-1][2]){
+            dirs[idx-1] = -dirs[idx];
+            idx--;
         }
 
-        //오른쪽으로 전파
-        for(int i=gearNum; i<3; i++){
-            int rightOfLeft = gear[i][idx[i]];
-            int leftOfRight = gear[i+1][(idx[i+1]+4)%8];
-            if(rightOfLeft == leftOfRight) break;
-            rot[i+1] = -rot[i];
+        idx = x;
+        while(idx < 3 && gear[idx][2] != gear[idx+1][6]){
+            dirs[idx+1] = -dirs[idx];
+            idx++;
         }
 
-        //기어 회전
         for(int i=0; i<4; i++){
-            idx[i] = (idx[i] -rot[i] + 8) % 8;
+            if(dirs[i] == 0)  continue;
+            rotate(i,dirs[i]);
         }
     }
-    public static void main(String args[]) throws IOException {
-        // 입력
-        for(int i=0; i<4; i++){
-            String line = br.readLine();
-            idx[i] = 2;
-            for(int j=0; j<8; j++){
-                gear[i][j] = line.charAt(j)-'0';
 
+    static void rotate(int x, int dir){
+        int[] tmp = new int[8];
+        for(int i=0; i<8; i++){
+            tmp[i] = gear[x][i];
+        }
+        if(dir == -1){
+            for(int i=0; i<8; i++){
+                gear[x][i] = tmp[(i+1)%8];
             }
         }
+        else{
+            for(int i=0; i<8; i++){
+                gear[x][i] = tmp[(i+7)%8];
+            }
+        }
+    }
+
+    static int cal() {
+        int score = 0;
+        for(int i=0; i<4; i++){
+            if(gear[i][0] == 1){
+                score += (1 << i);
+            }
+        }
+        return score;
+    }
+    public static void main(String[] args) throws IOException {
+        for(int i=0; i<4; i++){
+            String line = br.readLine();
+            for(int j=0; j<8; j++){
+                gear[i][j] = line.charAt(j) - '0';
+            }
+        }
+
         k = Integer.parseInt(br.readLine());
         for(int i=0; i<k; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x, y;
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            arr.add(new Point(x,y));
+            int n = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+            go(n-1,dir);
         }
-        // 작업 수행
-        for(int i=0; i<arr.size(); i++){
-            int gearNum = arr.get(i).x-1;
-            int dir = arr.get(i).y;
-            rot = new int[4];
-            rot[gearNum] = dir;
-            rotate(gearNum, dir);
-            
-        }
-        //점수 합산
-        int sum = 0;
-        for(int i=0; i<4; i++){
-            if(gear[i][(idx[i]+6)%8] == 1){
-                sum += (1<<i);
-            }
-        }
-        
-        bw.write(sum+"");
+        bw.write(cal()+"");
         bw.flush();
         bw.close();
     }
