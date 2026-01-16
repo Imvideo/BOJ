61eb8c81cd41ccd72847a13debea1198f42b203c
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2026-01-16 12:38:04 +0900

    [Silver I] Title: 트럭, Time: 116 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -2,58 +2,44 @@ import java.io.*;
 import java.util.*;
 import java.awt.Point;
 
-public class Main{
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n,w,L,ans=0;
-    static Queue<Integer> trucks = new LinkedList<Integer>();
+
+    static int n, w, l, idx = 1, sum = 0, cnt = 0;
+    static int[] truck = new int[1002];
     static int[] bridge = new int[102];
 
-    static boolean isBridgeEmpty(){
-        for(int i=0; i<w; i++){
-            if(bridge[i] != 0) return false;
-        }
-        return true;
-    }
-
-    static void go(){
-        for(int i=w-1; i>0; i--){
-            bridge[i] = bridge[i-1];
-        }
-        bridge[0] = 0;
-        return;
-    }
-
-    static int weightSum(){
-        int sum=0;
-        for(int i=0; i<w; i++){
-            sum+= bridge[i];
-        }
-        return sum;
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<n; i++) {
-            trucks.add(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i <= n; i++) {
+            truck[i] = Integer.parseInt(st.nextToken());
         }
-        
-        do{
-            int bridgeWeight = weightSum();
-            if(bridgeWeight <= L) {
-                bridgeWeight-=bridge[w-1];
-                go();
-                if(!trucks.isEmpty() && bridgeWeight+trucks.peek() <= L){
-                    bridge[0] = trucks.poll();
+        while (true) {
+            if (bridge[w] != 0){
+                sum -= truck[bridge[w]];
+                if(bridge[w] == n) {
+                    cnt++;
+                    break;
                 }
             }
-            ans++;
-        } while(!isBridgeEmpty());
-        bw.write(ans+"\n");
+            for (int i = w; i > 1; i--) {
+                bridge[i] = bridge[i - 1];
+            }
+            if (sum + truck[idx] <= l && idx <= n) {
+                bridge[1] = idx;
+                sum += truck[idx++];
+            }
+            else{
+                bridge[1] = 0;
+            }
+            cnt++;
+        }
+        bw.write(cnt + "");
         bw.flush();
         bw.close();
     }
