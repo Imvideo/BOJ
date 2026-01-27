b308c22393f9124ac4dc4b7425bb7a0ced400690
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2026-01-27 16:10:34 +0900

    [Gold IV] Title: 테트로미노, Time: 808 ms, Memory: 41752 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -7,35 +7,31 @@ public class Main {
 
     static int n, m, mx = 0;
     static int[][] board = new int[502][502];
-
-    static ArrayList<int[][]> tetro = new ArrayList<>();
-    static boolean nextPermutation(int[] brute){
-        int i = brute.length - 2;
-        while(i >= 0 && brute[i] >= brute[i+1]) i--;
+    static ArrayList<int[][]> shape = new ArrayList<>();
+    static boolean nextPermutation(int[] a){
+        int i = a.length-2;
+        while(i>=0 && a[i] >= a[i+1]) i--;
         if(i < 0) return false;
 
-        int j = brute.length - 1;
-        while(brute[i] >= brute[j]) j--;
+        int j = a.length - 1;
+        while(a[i] >= a[j]) j--;
+        int tmp = a[i]; a[i] = a[j]; a[j] = tmp;
 
-        int tmp = brute[j]; brute[j] = brute[i]; brute[i] = tmp;
-
-        for(int l = i+1, r = brute.length-1; l < r; l++, r--){
-            tmp = brute[l]; brute[l] = brute[r]; brute[r] = tmp;
+        for(int l = i+1, r = a.length-1; l < r; l++, r--){
+            tmp = a[l]; a[l] = a[r]; a[r] = tmp;
         }
         return true;
     }
 
-    static boolean isAdj(int ax, int ay, int bx, int by){
-        if(ax == bx) return Math.abs(ay - by) == 1;
-        if(ay == by) return Math.abs(ax - bx) == 1;
-        return false;
-    }
-    static boolean isCon(int[][] shape){
+    static boolean isConn(int[][] comb){
         boolean flag = false;
         for(int i=0; i<4; i++){
             int cnt = 0;
             for(int j=0; j<4; j++){
-                if(isAdj(shape[i][0], shape[i][1], shape[j][0], shape[j][1])) cnt++;
+                if(i==j) continue;
+                if(isAdj(comb[i][0], comb[i][1], comb[j][0], comb[j][1])){
+                    cnt++;
+                }
             }
             if(cnt == 0) return false;
             if(cnt >= 2) flag = true;
@@ -43,46 +39,60 @@ public class Main {
         return flag;
     }
 
+    static boolean isAdj(int ax, int ay, int bx, int by) {
+        if(ax == bx) return Math.abs(ay - by) == 1;
+        if(ay == by) return Math.abs(ax - bx) == 1;
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for(int j=0; j<m; j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int[] brute = new int[16];
-        for(int i=12; i<16; i++) brute[i] = 1;
+        for(int i=12; i<16; i++){
+            brute[i] = 1;
+        }
 
-        do{
-            int[][] shape = new int[4][2];
+        do {
             int idx = 0;
+            int[][] comb = new int[4][2];
             for(int i=0; i<16; i++){
-                if(brute[i] == 1) {
-                    shape[idx][0] = i / 4;
-                    shape[idx][1] = i % 4;
+                if(brute[i] == 1){
+                    comb[idx][0] = i / 4;
+                    comb[idx][1] = i % 4;
                     idx++;
                 }
             }
-            if(isCon(shape)) tetro.add(shape);
-        }while(nextPermutation(brute));
+            if(isConn(comb)) shape.add(comb);
+        } while(nextPermutation(brute));
 
-        for(int i=0; i<=n-4; i++){
-            for(int j=0; j<=m-4; j++){
-                for(int[][] shape : tetro){
+        for(int i=0; i <= n-4; i++){
+            for(int j=0; j<= m-4; j++){
+                for(int[][] tetro : shape){
                     int tot = 0;
                     for(int t=0; t<4; t++){
-                        tot += board[i+shape[t][0]][j+shape[t][1]];
+                        tot += board[i + tetro[t][0]][j + tetro[t][1]];
                     }
-                    mx = Math.max(mx, tot);
+                    if(mx < tot){
+                        mx = tot;
+                        // bw.write("mx = " + tot +"\n");
+                        // for(int t=0; t<4; t++){
+                        //     bw.write(tetro[t][0] + " " + tetro[t][1] +"\n");
+                        // }
+                        // bw.write("\n");
+                    }
                 }
             }
         }
-        bw.write(mx + "");
+        bw.write(mx+"");
         bw.flush();
         bw.close();
     }
