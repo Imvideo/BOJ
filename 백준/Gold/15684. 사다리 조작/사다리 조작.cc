11343a0d8ecd6b97e4a9d16653520b35a1cc1b1a
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2026-02-10 16:00:00 +0900

    [Gold III] Title: 사다리 조작, Time: 368 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -6,35 +6,36 @@ using namespace std;
 
 int n, m, h;
 int board[32][12];
-vector<pair<int, int>> coords;
+vector<pair<int,int>> ladder;
 
 bool check(){
-  for(int i=1; i<=n; i++){
-    int cur = i;
-    for(int j=1; j<=h; j++){
-      if(board[j][cur-1]) cur--;
-      else if(board[j][cur]) cur++;
+  for(int i=1; i<=n; i++) {
+    int a = 1;
+    int b = i;
+    while(a <= h) {
+      if(board[a][b-1] == 1) b--;
+      else if (board[a][b] == 1) b++;
+      a++;
     }
-    if(cur != i) return 0;
+    if(b != i) return false;
   }
-  return 1;
+  return true;
 }
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
   cin >> n >> m >> h;
-  for(int i=0; i<m; i++){
+  for(int i=0; i<m; i++)
+  {
     int a, b;
     cin >> a >> b;
     board[a][b] = 1;
   }
-
-  for(int i=1; i<=h; i++){
-    for(int j=1; j<n; j++){
-      if(board[i][j-1] || board[i][j] || board[i][j+1]) continue;
-      coords.push_back({i,j});
+  for(int i=1; i<=h; i++) {
+    for(int j=1; j<=n; j++) {
+      if(board[i][j-1] == 1 || board[i][j] == 1 || board[i][j+1] == 1) continue;
+      ladder.push_back({i,j});
     }
   }
 
@@ -44,24 +45,23 @@ int main(void){
   }
 
   int ans = 4;
-  int sz = coords.size();
+  int sz = ladder.size();
   for(int i=0; i<sz; i++){
-    board[coords[i].X][coords[i].Y] = 1;
+    board[ladder[i].X][ladder[i].Y] = 1;
     if(check()) ans = min(ans, 1);
     for(int j=i+1; j<sz; j++){
-      board[coords[j].X][coords[j].Y] = 1;
+      board[ladder[j].X][ladder[j].Y] = 1;
       if(check()) ans = min(ans, 2);
       for(int k=j+1; k<sz; k++){
-        board[coords[k].X][coords[k].Y] = 1;
+        board[ladder[k].X][ladder[k].Y] = 1;
         if(check()) ans = min(ans, 3);
-        board[coords[k].X][coords[k].Y] = 0;
+        board[ladder[k].X][ladder[k].Y] = 0;
       }
-      board[coords[j].X][coords[j].Y] = 0;
+      board[ladder[j].X][ladder[j].Y] = 0;
     }
-    board[coords[i].X][coords[i].Y] = 0;
+  board[ladder[i].X][ladder[i].Y] = 0;
   }
-  if(ans == 4) ans = -1;
-  cout << ans;
-  
+  if(ans == 4) cout << -1;
+  else cout << ans;
 }
-      
+
